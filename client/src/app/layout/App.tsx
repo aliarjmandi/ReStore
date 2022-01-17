4fd4272348afb854b2894c1d4b4519a68421ec66
aliarjmandi <aliarjmandi@gmail.com>
2022-01-17 21:51:36 +0330

    end of part 3

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,42 +1,48 @@
-
 import { Container, CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/system";
-import React from "react";
+import { createTheme } from "@mui/material/styles";
 import { useState } from "react";
+import {  Route,  Routes } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 function App() {
 
-  const [darkMode,setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
     palette: {
-      mode : paletteType,
-      background:{
-        default: paletteType ==='light' ? '#eaeaea' : '#121212'
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
     }
   })
 
+ 
 
-  function handleThemeChange()
-  {
+  function handleThemeChange() {
     setDarkMode(!darkMode);
 
   }
-
   return (
-    <>
-      <ThemeProvider theme={theme}>
-      </ThemeProvider>
-
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 export default App;
